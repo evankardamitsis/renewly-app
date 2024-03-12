ee59ecb2703be5477779aa1dad1ea9be5ef6af18
Kardamitsis Evan <kardamitsis.e@gmail.com>
2024-03-12 17:57:46 +0200

    fix types

--- src/api/projects/updateProject.ts
+++ src/api/projects/updateProject.ts
@@ -1,10 +1,17 @@
 import {supabase} from "../../../supabaseClient.ts";
+import {ProjectDetails} from "./createProject.ts";
 
-export async function updateProject(projectId: string, updates: never) {
+export async function updateProject(orgId:string | null, token:string | null, projectDetails : ProjectDetails, projectId:number) {
     const { data, error } = await supabase
-        .from('Projects')
-        .update(updates)
-        .match({ id: projectId });
-    if (error) throw error;
+        .from('projects')
+        .update({
+            ... projectDetails
+        })
+        .eq('id', projectId)
+        .eq('org_id', orgId)
+
+    if (error) throw new Error(error.message);
+
     return data;
+
 }
